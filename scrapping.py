0357bccc24130f8756e539f3325b83eab5a7ad93
nahuelavila4 <avilanahuel99@gmail.com>
2024-10-17 22:54:52 -0300

    1.1 Test Scrapping

--- scrapping.py
+++ scrapping.py
@@ -1,14 +1,21 @@
-import requests
+import requests, re
 from bs4 import BeautifulSoup
 
 url = "https://owasp.org/www-project-top-ten/"
 
 r = requests.get(url)
 
-if r.status_code == 200:
-    soup = BeautifulSoup(r.text, "html.parser")
-    machines = soup.findAll("li", stronger = True)
-    for machine in machines:
-        print(machine)
+def scrapp1():
+    if r.status_code == 200:
+        print("Vulnerabilities Titles")
+        soup = BeautifulSoup(r.text, "html.parser")
+        titles = soup.find_all("a", href=True)
+        filtered_titles = [link for link in titles if "https://owasp.org/Top10/A" in link['href']]
 
+        for t in filtered_titles:
+            print(t.text)
+    else:
+        print(f"Error: {r.status_code}")
 
+if __name__ == "__main__":
+    scrapp1()
