9133a2d7ec3e9acecc4ec65197f15ecb770fb41f
nahuel <avilanahuel99@gmail.com>
2024-10-22 19:31:49 -0300

    2. Parsing HTML

--- scrapping.py
+++ scrapping.py
@@ -1,19 +1,21 @@
 import requests, re
 from bs4 import BeautifulSoup
 
-url = "https://owasp.org/www-project-top-ten/"
-
-r = requests.get(url)
+url = "https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&search_type=all&isCpeNameSearch=false"
 
 def scrapp1():
+    r = requests.get(url)
     if r.status_code == 200:
-        print("Vulnerabilities Titles")
+        print("Security Vulnerabilities")
         soup = BeautifulSoup(r.text, "html.parser")
-        titles = soup.find_all("a", href=True)
-        filtered_titles = [link for link in titles if "https://owasp.org/Top10/A" in link['href']]
+        
+        vulns = soup.find("tbody")
+        with open("output233.txt", "w") as file:
+            for item in vulns:
 
-        for t in filtered_titles:
-            print(t.text)
+                plain_text = item.get_text()
+                file.write(plain_text)
+        print("El archivo de salida se ha guardado correctamente")
     else:
         print(f"Error: {r.status_code}")
 
