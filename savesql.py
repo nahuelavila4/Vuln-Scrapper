fab99e6d4325404adee333cf1e28d0eefe366711
nahuel <avilanahuel99@gmail.com>
2024-10-27 10:50:03 -0300

    4. Insert in DB Done

--- savesql.py
+++ savesql.py
@@ -1,21 +1,30 @@
 import sqlite3
 
-conn = sqlite3.connect("vulnerabilities.db")
+conn = sqlite3.connect("scraper.db")
 cursor = conn.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS vulnerabilities (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    cve_id TEXT,
-    description TEXT,
-    published_date TEXT
+    cve_id TEXT UNIQUE,
+    descrip TEXT,
+    date TEXT
 )
 ''')
 
-def save_sql(id, descrip, date):
-    print("Alo")
-
-
-
-
-
+def insert_vuln(cve, descrip, date):
+    cursor.execute(f"SELECT * FROM vulnerabilities WHERE cve_id = ?", (cve,))
+    result = cursor.fetchone()
+    if result is None:
+        try:
+            cursor.execute('''
+                INSERT INTO vulnerabilities(cve_id, descrip, date)
+                VALUES (?, ?, ?)     
+            ''', (cve, descrip, date))
+            print(f"Vulnerabilidad {cve} agregada a la base de datos")
+            conn.commit()
+        except Exception as e:
+            print(f"Error: {e}")
+    else:
+        print("Las vulnerabilidades ya se encuentran en la base de datos")
+    
--- scrapping.py
+++ scrapping.py
@@ -1,7 +1,8 @@
 import requests, re, savesql
-from savesql import save_sql
+from savesql import insert_vuln
 from bs4 import BeautifulSoup
 from datetime import datetime
+import sqlite3
 
 url = "https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&search_type=all&isCpeNameSearch=false"
 
@@ -11,32 +12,31 @@ def scrapp1():
         soup = BeautifulSoup(r.text, "html.parser")      
         vulns = soup.find_all("tr", attrs={"data-testid": True})
 
-        with open("output2B.txt", "w") as file:
+        with open("output.txt", "w") as file:
             #file.write("National Vulnerability Database")
             for item in vulns:
                 versions = item.find_all("td", attrs={"nowrap": "nowrap"})
                 for v in versions:    
                     v.extract()
 
-                # Obtener ID
+                # Obtener ID y descrip
                 id_vuln = item.find("a").get_text().strip()
                 descrip_vuln = item.find("p").get_text().strip()
-                # Obtener fecha - convertir luego con datetime
+                # Obtener fecha
                 date_vuln = item.find("span").get_text().strip()
                 plain_text = item.get_text()
-                #save_sql(id_vuln, descrip_vuln, date_vuln)
-                #file.write(plain_text)
+                file.write(plain_text)
 
-                # Formato fecha date_vuln October 26, 2024; 6:15:10 AM -0400
                 date_vuln = date_vuln[:-6]
                 date_time_obj = datetime.strptime(date_vuln, "%B %d, %Y; %I:%M:%S %p")
                 formated_date = date_time_obj.strftime("%m/%d/%Y %H:%M:%S")
-                print(type(formated_date))
-                print(formated_date)
+
+                conn = sqlite3.connect("scraper.db")
+                insert_vuln(id_vuln, descrip_vuln, formated_date)
+                conn.close()
         print("El archivo de salida se ha guardado correctamente")
     else:
         print(f"Error al extraer: {r.status_code}")
 
 if __name__ == "__main__":
-    #save_sql()
     scrapp1()
